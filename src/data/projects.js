29828b5c9e43539207d683d5b4b795c185e61263
aleasegur <aleasegur@alu.edu.gva.es>
2026-02-12 12:05:28 +0100

    actualizado las imagenes y cambio de un proyecto

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,20 @@
 export const projects = [
     {
         id: 1,
-        title: 'Portfolio personal',
-        description: "Mi primer portfolio creado con React.",
-        image: "/images/proyecto1.png"
+        title: 'Star wars guessing game',
+        description: "JUego de adivinar un personaje de Star wars.",
+        image: "/images/proyecto1.webp"
     },
     {
         id: 2,
         title: 'App de gestion de proyectos',
         description: "App sencilla para gestionar proyectos.",
-        image: "/images/proyecto2.png"
+        image: "/images/proyecto2.webp"
     },
     {
         id: 3,
         title: 'Web ODS 1',
         description: "Diseño de una web para solucionar el ODS1.",
-        image: "/images/proyecto3.png"
+        image: "/images/proyecto3.webp"
     }
 ];
